3696689ad6ff71bd64802a8d3fdc0832ce63cf3a
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-04-30 16:19:19 +0200

    Added fetch products logic to shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-// import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './pages/Navbar'
-// import Home from './pages/Home'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,47 @@
+import "../styles/Shop.css";
+import { useState, useEffect } from "react";
+
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      const data = await response.json();
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error ferching products:', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="shop">
-      <p>products will appear here</p>
+      <div className="productsGrid">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <img src={product.image} alt={product.title} />
+              <p>Price: ${product.price}</p>
+            </li>
+          ))}
+        </ul>
+        )}
+      </div>
     </div>
   )
 }
