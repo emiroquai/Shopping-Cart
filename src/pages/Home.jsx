a9d40c4d65d80e3a51a1627282f9cffdc4cbbc47
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-04-30 15:23:23 +0200

    Added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to RoRoMart!</h1>
+      <p>Discover our fake selection of amazing products. Delivered from the interwebs to your heart.</p>
+      <a className="linkButton">Shop Collection</a>
+    </div>
+  )
+}
+
+export default Home;
